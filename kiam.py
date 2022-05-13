kiam.scrs2mer and kiam.mer2scrs corrected: chunk introduced to solve the stack overflow problem.

kiam.py
@@ -126,11 +126,31 @@ def scrs2pa(xscrs, jd, grad_req=False):
     out = fkt.translations.kscrs2pa(xscrs, jd)
     return _return_if_grad_req(out, grad_req)
 def scrs2mer(xscrs, jd, grad_req=False):
-    out = fkt.translations.kscrs2mer(xscrs, jd)
-    return _return_if_grad_req(out, grad_req)
+    chunk = 10000
+    dim = xscrs.shape[0]
+    ncols = xscrs.shape[1]
+    xmer = np.empty((dim, ncols))
+    dxmer = np.empty((dim, dim, ncols))
+    for i in range(int(np.ceil(ncols/chunk))):
+        cols = np.arange(i*chunk, min((i+1)*chunk, ncols))
+        xmer[:, cols], dxmer[:, :, cols] = fkt.translations.kscrs2mer(xscrs[:, cols], jd[cols])
+    if grad_req:
+        return xmer, dxmer
+    else:
+        return xmer
 def mer2scrs(xmer, jd, grad_req=False):
-    out = fkt.translations.kmer2scrs(xmer, jd)
-    return _return_if_grad_req(out, grad_req)
+    chunk = 10000
+    dim = xmer.shape[0]
+    ncols = xmer.shape[1]
+    xscrs = np.empty((dim, ncols))
+    dxscrs = np.empty((dim, dim, ncols))
+    for i in range(int(np.ceil(ncols / chunk))):
+        cols = np.arange(i * chunk, min((i + 1) * chunk, ncols))
+        xscrs[:, cols], dxscrs[:, :, cols] = fkt.translations.kmer2scrs(xmer[:, cols], jd[cols])
+    if grad_req:
+        return xscrs, dxscrs
+    else:
+        return xscrs
 def scrs2gcrs(xscrs, jd, DistUnit, VelUnit):
     return fkt.translations.kscrs2gcrs(xscrs, jd, DistUnit, VelUnit)
 def gcrs2scrs(xgcrs, jd, DistUnit, VelUnit):

main.py
@@ -1,9 +1,10 @@
 import numpy as np
 import kiam
 import Trajectory
+from numpy import sqrt
 
 t0 = 0.0
-s0 = np.array([1.0, 0.0, 0.0, 0.0, 1.0, 0.0])
+s0 = np.array([2.0, 0.0, 0.0, 0.0, 1/sqrt(2.0), 0.0])
 jd0 = kiam.juliandate(2022, 4, 30, 0, 0, 0)
 tr = Trajectory.Trajectory(s0, t0, jd0, 'rv', 'scrs', 'moon')
 tr.set_model('rv', 'nbp', 'moon', [])
@@ -11,12 +12,7 @@ tr.model['data']['jd_zero'] = jd0
 tr.model['data']['mass'] = 100.0
 tr.model['data']['area'] = 2.0
 tr.model['data']['order'] = 1
-tr.propagate(2*np.pi, 10)
+tr.propagate(2*np.pi, 200000)
+tr.change_system('mer')
 
-print(tr)
-
-kiam.save(tr, 'trajectory')
-
-tr2 = kiam.load('trajectory')
-
-print(tr2)
+tr.show('3d')